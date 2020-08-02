4c6a2ab74f177cd0d95e00721074ccbc15c78470
omniclause <vaughnmurphy89@gmail.com>
2020-08-02 15:33:08 -0500

    added burgers_db and burgers table. populated burgers table with seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The Krusty Burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Steamed Ham', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Paunch Burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The Big Kahuna', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Good Burger', FALSE);
